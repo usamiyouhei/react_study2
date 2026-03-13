f53a9544ae20045e70da2596c22724e95f2064eb
youhei usami <usao9110@icloud.com>
2026-03-13 10:01:13 +0400

    3/13

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 // import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Link to='/sample-page' targget="_blank">Sample Pageへ！</Link>
     </div>
   )
 }
