e7ef98118644b7baa713950291f6f491cf4a3056
youhei usami <usao9110@icloud.com>
2026-03-16 10:57:39 +0400

    3/15

--- src/pages/QuizHome.jsx
+++ src/pages/QuizHome.jsx
@@ -1,7 +1,15 @@
 // import React from "react";
 
+import { Link } from "react-router-dom";
+import { ROUTES } from "../const";
+
 const QuizHome = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1>Quiz App</h1>
+      <Link to={ROUTES.QUIZ}>Start</Link>
+    </>
+  );
 };
 
 export default QuizHome;
