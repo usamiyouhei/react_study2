c71c1a534644f0d6e9514faf8aaa8280deb08fe2
youhei usami <usao9110@icloud.com>
2026-03-18 10:47:17 +0400

    3/18

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 import QuizButton from "../component/Button/QuizButton";
 import QuizDesplay from "../component/QuizDisplay/QuizDesplay";
 import quizData from "../data/quiz";
+import { useNavigate } from "react-router-dom";
 
 const QuizPage = () => {
   const [quizIndex, setQuizIndex] = useState(0);
   const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
 
   const handleClick = (clickedIndex) => {
     if (clickedIndex === quizData[quizIndex].answerIndex) {
