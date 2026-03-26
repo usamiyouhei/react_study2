f209336951080e04cbea0bdd5a245236ecb20a09
youhei usami <usao9110@icloud.com>
2026-03-26 12:02:02 +0400

    3/26

--- src/component/TaskList/TaskListContainer.tsx
+++ src/component/TaskList/TaskListContainer.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Priority, Task } from "../../domain/Task";
 import TaskFormPresenter from "../TaskForm/TaskFormPresenter";
+import TaskListPresenter from "./TaskListPresenter";
 
 const TaskListContainer: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -15,9 +16,28 @@ const TaskListContainer: React.FC = () => {
     setTasks([...tasks, newTask]);
   };
 
+  const handleToggleTask = (id: string) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task,
+      ),
+    );
+  };
+
+  const handleDeleteTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="task-container">
       <TaskFormPresenter onAddTask={handleAddTask} />
+      <TaskListPresenter
+        tasks={tasks}
+        onToggleTask={handleToggleTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 };
+
+export default TaskListContainer;
