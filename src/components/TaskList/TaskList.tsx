3c14b3daad867f189089e613deaed5c41d6211e0
youhei usami <usao9110@icloud.com>
2026-04-09 12:08:16 +0400

    4/9

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -12,39 +12,15 @@ const TaskListPresenter: React.FC<TaskListPresenterProps> = ({
   onToggleTask,
   onDeleteTask,
 }) => {
+  if (tasks.length === 0)
+    return <div className="empty-message">タスクはありません</div>;
   return (
     <ul className="task-list">
       {tasks.map((task) => (
         <li
           key={task.id}
           className={`task-item ${task.completed ? "completed" : ""}`}
-        >
-          <div>
-            <span className={`task-priority priority-${task.priority}`}>
-              {task.priority}
-            </span>
-            <span className="task-title">{task.title}</span>
-          </div>
-          <div className="task-actions">
-            <button
-              className="toggle-btn"
-              onClick={() => onToggleTask(task.id)}
-              aria-label={
-                task.completed ? "タスクを未完了にする" : "タスクを完了にする"
-              }
-            >
-              {/* {task.completed ? "未完了" : "完了"} */}
-              <FaCheck />
-            </button>
-            <button
-              className="delete-btn"
-              onClick={() => onDeleteTask(task.id)}
-              aria-label="タスク削除"
-            >
-              <FaTrash />
-            </button>
-          </div>
-        </li>
+        ></li>
       ))}
     </ul>
   );
